867b1abed6357ebf65c1e32debb1b9287308a9c6
johanna holm <johanna.holm@ki.se>
2013-01-19 22:12:23 +0100

    second commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name='biostuff',
+    version='0.1dev',
+    description='Python course 2013',
+    author='johhol',
+    author_email='johanna.holm@ki.se',
+    url='http://www.github.com/johhol/biostuff',
+    packages=['biostuff'],
+    license='GPLv3',
+    long_description=open('README.txt').read(),
+    )
